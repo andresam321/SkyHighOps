2e70dcce82e675e80fd24965d70de36e4cfc043f
Andres Mercado <andresmercado96@gmail.com>
2024-05-29 08:17:55 -0700

    updated users

--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,27 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import uuid
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.String(4), nullable=False, unique=True, default=lambda: str(uuid.uuid4()))
+    firstname = db.Column(db.String(40), nullable=False)
+    lastname = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        if not self.employee_id:
+            self.employee_id = str(uuid.uuid4())
 
     @property
     def password(self):
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,18 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        firstname='De',lastname='mo',username='Demo', email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        firstname='mar',lastname='nie',username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        firstname='bo',lastname='bbie',username='bobbie', email='bobbie@aa.io', password='password')
+    andres = User(
+        firstname='and',lastname='res',username='andres', email='andres@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(andres)
     db.session.commit()
 
 
